d6dfdb5bcf62f2ef43287f199f3c5e93486bdbe7
Lennart-Meyer <lennart.meyer@student.jade-hs.de>
2020-09-28 15:24:51 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtes6t
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.0 ;
+            bool isMale = true;
+            
+            Console.WriteLine("GiraffeAcadamy");
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
